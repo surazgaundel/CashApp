f7a0d35d64243df4e7cd5e455ddcca0f4e095c59
surazgaundel <gaundelsuraz99@gmail.com>
2023-08-03 16:40:21 +0530

    Initial Cash card component

--- src/components/Banking/Banking.jsx
+++ src/components/Banking/Banking.jsx
@@ -1,29 +1,43 @@
 import React from 'react'
-import BankingPhoneImage from '/src/assets/Banking/banking-phone.png';
+import BankingPhone from '/src/assets/Banking/banking-phone.png';
+import BankingColumn from '/src/assets/Banking/banking-column.png';
+import BankingCubes from '/src/assets/Banking/banking-cubes.png';
+import BankingHole from '/src/assets/Banking/banking-hole.png';
+import BankingMonster from '/src/assets/Banking/banking-monster.png';
+import BankingRamp1 from '/src/assets/Banking/banking-ramp-1.png';
+import BankingRamp2 from '/src/assets/Banking/banking-ramp-2.png';
+import BankingStairs1 from '/src/assets/Banking/banking-stairs-1.png';
+import BankingStairs2 from '/src/assets/Banking/banking-stairs-2.png';
+import BankingTrack1 from '/src/assets/Banking/banking-track-1.png';
+import BankingTrack2 from '/src/assets/Banking/banking-track-2.png';
+import BankingTube from '/src/assets/Banking/banking-tube.png';
 export default function Banking() {
   return (
     <div id='banking'>
-    <div className='flex items-center justify-start w-[60%] m-auto h-[85vh]'>
-      <section className='flex flex-col w-1/2 items-start'>
+    <div className='flex items-center justify-start w-[60%] ml-[10%] h-[100vh]'>
+      <section className='flex flex-col w-1/2 items-start z-20'>
       <h1 className='text-3xl font-bolder tracking-wider'>Banking</h1>
       <p className="font-['Mulish'] text-black  leading-6">Receive your paycheck, tax returns, and other direct deposits up to two days early using your Cash App routing 
 and account number.</p>
       </section>
     <div className="flex items-center justify-center">
-      <img src={BankingPhoneImage} className="h-[40vh]"  alt='banking phone image'/>  
+      <img src={BankingPhone} className="h-[40vh] z-10"  alt='banking phone image'/>  
     </div>
     </div>
-    {/* <div className=" flex">
-      <img src={BigPillarImage} className='absolute bottom-0' alt='big pillar image'/>
-      <img src={SmallPillarImage} className='absolute bottom-16 start-[26%]'alt='small pillar image'/>
-      <img src={MediumPillarImage} className='absolute bottom-6 left-72' alt='medium pillar image'/>
-      <img src={MediumPillarImage} className='absolute start-[44%] bottom-10 z-10' alt='medium pillar image'/>
-      <img src={MediumPillarImage} className='absolute end-[15%] bottom-[15%] z-0' alt='medium pillar image'/>
-      <img src={MediumPillarImage} className='absolute start-[52%] bottom-0 z-10' alt='medium pillar image'/>
-      <img src={LargePillarImage} className='absolute start-[50%] bottom-[15%] z-0' alt='large pillar image'/>
-      <img src={ColumnImage} className='absolute bottom-5 right-0' alt='column image'/>
-      <img src={Column2Image} className='absolute bottom-0 h-1/2 right-[20%] z-10' alt='column2 image'/>
-    </div> */}
+    <div className=" flex">
+      <img src={BankingColumn} className='absolute top-[10%] left-[5%] z-10' alt='column'/>
+      <img src={BankingCubes} className='absolute bottom-[10%] end-[25%]'alt='cubes'/>
+      <img src={BankingHole} className='absolute top-0 left-0' alt='hole1'/>
+      <img src={BankingHole} className='absolute -bottom-[17%] right-[15%] z-0' alt='hole2'/>
+      <img src={BankingMonster} className='absolute end-[5%] bottom-[5%] z-10' alt='monster'/>
+      <img src={BankingRamp1} className='absolute top-[35%] right-[25%] z-0' alt='ramp1'/>
+      <img src={BankingRamp2} className='absolute start-[15%] bottom-[5%] z-10' alt='ramp2'/>
+      <img src={BankingStairs1} className='absolute right-[5%] top-[40%] z-0' alt='stairs1'/>
+      <img src={BankingStairs2} className='absolute bottom-[5%] left-[5%] right-0' alt='stairs2'/>
+      <img src={BankingTrack1} className='absolute top-[20%] start-[26%] z-0' alt='track1'/>
+      <img src={BankingTrack2} className='absolute top-[5%] right-[15%] h-1/4 z-0' alt='track2'/>
+      <img src={BankingTube} className='absolute bottom-0 left-[40%] h-[10vh]  z-0' alt='tube'/>
+    </div>
   </div>
   )
 }
--- src/components/CashCard/CashCard.jsx
+++ src/components/CashCard/CashCard.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
+import BoostPhone from '/src/assets/CashCard/boost-phone.png';
+import BoostBurger from '/src/assets/CashCard/boost-burger.png';
+import BoostCard from '/src/assets/CashCard/boost-card.png';
+import BoostCoffee from '/src/assets/CashCard/boost-coffee.png';
+import BoostHand from '/src/assets/CashCard/boost-hand.png';
+import BoostShoe from '/src/assets/CashCard/boost-shoe.png';
+import BoostStairs1 from '/src/assets/CashCard/boost-stairs-1.png';
+import BoostStairs2 from '/src/assets/CashCard/boost-stairs-2.png';
 
 export default function CashCard() {
   return (
     <div id='cashcard'>
-        <div className='flex items-center justify-center h-[85vh]'>
-      <section className='flex flex-col w-1/4 items-start -mr-28 '>
-      <h1 className='text-2xl font-bolder tracking-wider'>Cash Card & Boost</h1>
-      <p className="font-['Mulish'] text-white text-sm">The Cash Card is a free, customizable debit card that lets you pay online and in stores. It’s the only way to get Boosts—instant discounts that work at places where you want to spend.</p>
+    <div className='flex items-center justify-start w-[60%] ml-[10%] h-[100vh]'>
+      <section className='flex flex-col w-1/2 items-start z-20'>
+      <h1 className='text-3xl font-bolder tracking-wider'>Cash Card & Boost</h1>
+      <p className="font-['Mulish'] text-white leading-6">The Cash Card is a free, customizable debit card that lets you pay online and in stores. It’s the only way to get Boosts—instant discounts that work at places where you want to spend.</p>
       </section>
-    <div className="flex items-center justify-center -ml-40">
-      {/* <img src={MobileImage}  alt='mobile image'/>   */}
+    <div className="flex items-center justify-center">
+      <img src={BoostPhone} className=" z-10"  alt='boost phone image'/>  
     </div>
     </div>
-    {/* <div className=" flex">
-      <img src={BigPillarImage} className='absolute bottom-0' alt='big pillar image'/>
-      <img src={SmallPillarImage} className='absolute bottom-16 start-[26%]'alt='small pillar image'/>
-      <img src={MediumPillarImage} className='absolute bottom-6 left-72' alt='medium pillar image'/>
-      <img src={MediumPillarImage} className='absolute start-[44%] bottom-10 z-10' alt='medium pillar image'/>
-      <img src={MediumPillarImage} className='absolute end-[15%] bottom-[15%] z-0' alt='medium pillar image'/>
-      <img src={MediumPillarImage} className='absolute start-[52%] bottom-0 z-10' alt='medium pillar image'/>
-      <img src={LargePillarImage} className='absolute start-[50%] bottom-[15%] z-0' alt='large pillar image'/>
-      <img src={ColumnImage} className='absolute bottom-5 right-0' alt='column image'/>
-      <img src={Column2Image} className='absolute bottom-0 h-1/2 right-[20%] z-10' alt='column2 image'/>
-    </div> */}
-    </div>
+    <div className=" flex">
+      <img src={BoostBurger} className='absolute top-[10%] left-[5%] z-10' alt='burger'/>
+      <img src={BoostCard} className='absolute top-[28%] right-[20%] z-10'alt='card'/>
+      <img src={BoostCoffee} className='absolute bottom-[15%] right-[20%] z-10'  alt='coffee'/>
+      <img src={BoostHand} className='absolute top-[12%] right-[13%] z-10' alt='hand'/>
+      <img src={BoostShoe} className='absolute end-[5%] bottom-[5%] z-10' alt='shoe'/>
+      <img src={BoostStairs1} className='absolute bottom-0 right-0 h-screen z-0' alt='stairs1'/>
+      <img src={BoostStairs2} className='absolute left-0 bottom-0 z-0' alt='stairs2'/>
+      </div>
+  </div>
   )
 }
